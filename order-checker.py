1dec31413236e069e57b7ac0de0bebc918cfbc1b
ryan9918 <ryan9918@users.noreply.github.com>
2017-04-30 19:57:26 +0100

    added DE support and fixed shit

--- order-checker.py
+++ order-checker.py
@@ -13,18 +13,52 @@ def getOrderDetailsUK(number, email):
 	r = s.post(url, data=data, headers=headers)
 	soup = bs(r.content, "html.parser")
 	try:
-		delivery_date = soup.find('div', {'class': 'shipping-time'})
 		items = soup.find_all('div', {'class': 'order-step selected'})
 		for item in items:
 			status = item.find('div', {'class': 'order-step-content-wrp'})
 			status = (status.text).strip()
-		if str(status) != "Order processing":
-			delivery_date_parts = (delivery_date.text).split("\n")
-			delivery_date = delivery_date_parts[2].strip()
-			delivery_time_1 = delivery_date_parts[3].strip(" -")
-			delivery_time_2 = delivery_date_parts[4].strip()
-			print("Order {} for {} will be delivered on {} between {} and {} [{}]".format(number, email, delivery_date[:-1], delivery_time_1, delivery_time_2, status))
+		if str(status) == "Order confirmed, waiting to be packed":
+			print("Order {} for {} is confirmed and waiting to be packed".format(number, email))
 			confirmed_orders.append(number)
+		elif "Shipped" in str(status):
+			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
+			confirmed_orders.append(number)
+		elif str(status) == "Preparing shipment":
+			print("Order {} for {} is being prepared for shipping".format(number, email))
+			confirmed_orders.append(number)
+		elif str(status) == "Order processing":
+			print("Order {} for {} has not been confirmed yet and is still processing".format(number, email))
+	except:
+		print("Order {} for {} has not shipped or is an invalid input".format(number, email))
+
+
+# LOOOL IDK WHAT SOME OF THE GERMAN TRANSALTES TO SMH
+def getOrderDetailsDE(number, email):
+	r = s.get("https://www.adidas.de/bestellverfolgung", headers=headers)
+	soup = bs(r.content, "html.parser")
+	url = soup.find('form', {'id': 'dwfrm_ordersignup'})['action']
+	data = {'dwfrm_ordersignup_orderNo': number,
+	'dwfrm_ordersignup_email': email,
+	'dwfrm_ordersignup_signup': 'BESTELLUNG ANSEHEN'}
+	r = s.post(url, data=data, headers=headers)
+	soup = bs(r.content, "html.parser")
+	try:
+		items = soup.find_all('div', {'class': 'order-step selected'})
+		for item in items:
+			status = item.find('div', {'class': 'order-step-content-wrp'})
+			status = (status.text).strip()
+			print(status)
+		if str(status) == "Order confirmed, waiting to be packed":
+			print("Order {} for {} is confirmed and waiting to be packed".format(number, email))
+			confirmed_orders.append(number)
+		elif "Verschickt" in str(status):
+			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
+			confirmed_orders.append(number)
+		elif str(status) == "Der Versand wird vorbereitet":
+			print("Order {} for {} is being prepared for shipping".format(number, email))
+			confirmed_orders.append(number)
+		elif str(status) == "Bestellung wird bearbeitet":
+			print("Order {} for {} has not been confirmed yet and is still processing".format(number, email))
 	except:
 		print("Order {} for {} has not shipped or is an invalid input".format(number, email))
 
@@ -47,8 +81,11 @@ def getOrderDetailsUS(number, email):
 			confirmed_orders.append(number)
 		elif str(status) == "Order processing":
 			print("Order {} for {} has not been confirmed yet and is still processing".format(number, email))
-		elif str(status) == "Shipped":
+		elif "Shipped" in str(status):
 			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
+			confirmed_orders.append(number)
+		elif str(status) == "Preparing shipment":
+			print("Order {} for {} is being prepared for shipping".format(number, email))
 			confirmed_orders.append(number)
 	except:
 		print("Order {} for {} is an invalid input".format(number, email))
@@ -58,7 +95,7 @@ def getOrderDetailsUS(number, email):
 s = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
 'Origin': 'http://www.adidas.co.uk'}
-locale = input("LOCALE (US/UK):	")
+locale = input("LOCALE (US/UK/DE):	")
 confirmed_orders = []
 f = open("orders.txt", "r")
 for item in f.read().splitlines():
@@ -66,6 +103,8 @@ for item in f.read().splitlines():
 		parts = item.split(":")
 		if locale.upper() == "UK":
 			getOrderDetailsUK(parts[0], parts[1])
+		elif locale.upper() == "DE":
+			getOrderDetailsDE(parts[0], parts[1])
 		else:
 			getOrderDetailsUS(parts[0], parts[1])
 f.close()
