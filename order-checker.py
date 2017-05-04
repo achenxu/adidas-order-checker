ce773c7792af94714c5d508e0b9d9bfc263ed737
ryan9918 <ryan9918@users.noreply.github.com>
2017-05-04 20:31:49 +0100

    added FR support and general improvements

--- order-checker.py
+++ order-checker.py
@@ -23,10 +23,40 @@ def getOrderDetailsUK(number, email):
 		elif "Shipped" in str(status):
 			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
 			shipped_orders.append(number)
+			confirmed_orders.append(number)
 		elif str(status) == "Preparing shipment":
 			print("Order {} for {} is being prepared for shipping".format(number, email))
 			confirmed_orders.append(number)
 		elif str(status) == "Order processing":
+			print("Order {} for {} has not been confirmed yet and is still processing".format(number, email))
+	except:
+		print("Order {} for {} has not shipped or is an invalid input".format(number, email))
+
+def getOrderDetailsFR(number, email):
+	r = s.get("https://www.adidas.fr/suivi-commande", headers=headers)
+	soup = bs(r.content, "html.parser")
+	url = soup.find('form', {'id': 'dwfrm_ordersignup'})['action']
+	data = {'dwfrm_ordersignup_orderNo': number,
+	'dwfrm_ordersignup_email': email,
+	'dwfrm_ordersignup_signup': 'VOIR MA COMMANDE'}
+	r = s.post(url, data=data, headers=headers)
+	soup = bs(r.content, "html.parser")
+	try:
+		items = soup.find_all('div', {'class': 'order-step selected'})
+		for item in items:
+			status = item.find('div', {'class': 'order-step-content-wrp'})
+			status = (status.text).strip()
+		if str(status) == "Commande confirmée, en attente d'être préparée":
+			print("Order {} for {} is confirmed and waiting to be packed".format(number, email))
+			confirmed_orders.append(number)
+		elif "Expédiée" in str(status):
+			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
+			shipped_orders.append(number)
+			confirmed_orders.append(number)
+		elif str(status) == "Expédition en cours de préparation":
+			print("Order {} for {} is being prepared for shipping".format(number, email))
+			confirmed_orders.append(number)
+		elif str(status) == "Commande en cours de traitement":
 			print("Order {} for {} has not been confirmed yet and is still processing".format(number, email))
 	except:
 		print("Order {} for {} has not shipped or is an invalid input".format(number, email))
@@ -51,6 +81,7 @@ def getOrderDetailsDE(number, email):
 		elif "Verschickt" in str(status):
 			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
 			shipped_orders.append(number)
+			confirmed_orders.append(number)
 		elif str(status) == "Der Versand wird vorbereitet":
 			print("Order {} for {} is being prepared for shipping".format(number, email))
 			confirmed_orders.append(number)
@@ -81,6 +112,7 @@ def getOrderDetailsUS(number, email):
 		elif "Shipped" in str(status):
 			print("Order {} for {} has been shipped and is on its way to you!".format(number, email))
 			shipped_orders.append(number)
+			confirmed_orders.append(number)
 		elif str(status) == "Preparing shipment":
 			print("Order {} for {} is being prepared for shipping".format(number, email))
 			confirmed_orders.append(number)
@@ -92,7 +124,7 @@ def getOrderDetailsUS(number, email):
 s = requests.Session()
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
 'Origin': 'http://www.adidas.co.uk'}
-locale = input("LOCALE (US/UK/DE):	")
+locale = input("LOCALE (US/UK/DE/FR):	")
 confirmed_orders = []
 shipped_orders = []
 f = open("orders.txt", "r")
@@ -103,6 +135,8 @@ for item in f.read().splitlines():
 			getOrderDetailsUK(parts[0], parts[1])
 		elif locale.upper() == "DE":
 			getOrderDetailsDE(parts[0], parts[1])
+		elif locale.upper() == "FR":
+			getOrderDetailsFR(parts[0], parts[1])
 		else:
 			getOrderDetailsUS(parts[0], parts[1])
 f.close()
@@ -113,7 +147,7 @@ else:
 	print("\nYou have {} confirmed orders!".format(count))
 count = len(shipped_orders)
 if count > 5:
-	print("\nYou have {} shipped orders, coooook!".format(count))
+	print("\nYou have {} confirmed orders that have shipped, coooook!".format(count))
 else:
-	print("\nYou have {} shipped orders!".format(count))
+	print("\nYou have {} confirmed orders that have shipped!".format(count))
 input("\npress enter to close...")
